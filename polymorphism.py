4d1a044d3a5a482657188de3792bbbce290f7281
sibis <sinisenthilk@gmail.com>
2025-04-24 20:57:57 -0400

    update

--- polymorphism.py
+++ polymorphism.py
@@ -1,1 +1,23 @@
-dskhfkjf
+class Animal:
+    def speak(self):
+        raise NotImplementedError("Subclass must implement abstract method")
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow!"
+
+class Bird(Animal):
+    def speak(self):
+        return "Chirp!"
+
+def animal_speak(animal: Animal):
+    print(animal.speak())
+
+animals = [Dog(), Cat(), Bird()]
+
+for a in animals:
+    animal_speak(a)
