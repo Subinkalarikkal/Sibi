7411113f9aceab928b71d822d9b512ffbc8994c0
sibis <sinisenthilk@gmail.com>
2025-05-01 20:33:27 -0400

    update

--- polymorphism.py
+++ polymorphism.py
@@ -1,6 +1,64 @@
+# class Animal:
+#     def speak(self):
+#         raise NotImplementedError("Subclass must implement abstract method")
+#
+# class Dog(Animal):
+#     def speak(self):
+#         return "Woof!"
+#
+# class Cat(Animal):
+#     def speak(self):
+#         return "Meow!"
+#
+# class Bird(Animal):
+#     def speak(self):
+#         return "Chirp!"
+#
+# def animal_speak(animal: Animal):
+#     print(animal.speak())
+#
+# animals = [Dog(), Cat(), Bird()]
+#
+# for a in animals:
+#     animal_speak(a)
+
+
+
+
+
+# class Car:
+#     def drive(self):
+#         return "The car drives."
+#
+# class SportsCar(Car):
+#     def drive(self):
+#         return "The sports car zooms down the highway!"
+#
+# class ElectricCar(Car):
+#     def drive(self):
+#         return "The electric car glides silently."
+#
+# class Truck(Car):
+#     def drive(self):
+#         return "The truck rumbles down the road with a heavy load."
+#
+# def test_drive(vehicle: Car):
+#     print(vehicle.drive())
+#
+#
+# vehicles = [SportsCar(), ElectricCar(), Truck()]
+#
+#
+# for v in vehicles:
+#     test_drive(v)
+
+
+
+
+
 class Animal:
     def speak(self):
-        raise NotImplementedError("Subclass must implement abstract method")
+        pass
 
 class Dog(Animal):
     def speak(self):
@@ -10,14 +68,8 @@ class Cat(Animal):
     def speak(self):
         return "Meow!"
 
-class Bird(Animal):
-    def speak(self):
-        return "Chirp!"
-
-def animal_speak(animal: Animal):
+def make_animal_speak(animal):
     print(animal.speak())
 
-animals = [Dog(), Cat(), Bird()]
-
-for a in animals:
-    animal_speak(a)
+make_animal_speak(Dog())
+make_animal_speak(Cat())
--- sample.py
+++ sample.py
@@ -1,11 +1,25 @@
-a=int(input("Enter the number : "))
-print(a)
-print("abc")
-print("Sibi")
-a=0
-print("My name is Sibi")
-print("My nam is Subin")
-print("Coding")
-print("Computer")
-print("ABC")
-print("QWERTY")
+class Car:
+    def drive(self):
+        return "The car drives."
+
+class SportsCar(Car):
+    def drive(self):
+        return "The sports car zooms down the highway!"
+
+class ElectricCar(Car):
+    def drive(self):
+        return "The electric car glides silently."
+
+class Truck(Car):
+    def drive(self):
+        return "The truck rumbles down the road with a heavy load."
+
+def test_drive(vehicle: Car):
+    print(vehicle.drive())
+
+# List of different types of cars
+vehicles = [SportsCar(), ElectricCar(), Truck()]
+
+# Polymorphic behavior in action
+for v in vehicles:
+    test_drive(v)
