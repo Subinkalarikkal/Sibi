296650e3b4aededa708e71d2162f34479dfbc5c1
Subin Kalarikkal <subinkalarikkal120@gmail.com>
2025-05-16 05:38:54 +0530

    update

---
+++ Revision.py
@@ -0,0 +1,37 @@
+# 1 - Write a program to get two integer and a float from user and find the smallest number
+# 2 - Write a program to get two float from user and find the largest number
+# 3 - Write a program to print the even numbers between 10 and 20
+# 4 - Write a program to print the odd numbers 15 and 25
+# 5 - Write a program to get a character from user
+# 6 - Write a program to get a string from user
+# 7 - Write a program to get three float from user and find the smallest number.
+# 8 - Ask the user for the number of months, days, hours, minutes, and seconds they spent on an activity (like playing or studying). Calculate the total time in seconds.
+# 9 - Write a program to calculate the total cost of movie tickets. Ask the user how many adults and children are going, then multiply by the ticket prices (e.g., $10 for adults, $7 for children).
+# 10 - Savings Tracker
+# Ask the user to input the amount of money saved daily for 10 days.
+#
+# -> Calculate the total savings and average savings per day.
+# -> Display results based on the average:
+#   Average ≥ $20: "Excellent Saver!"
+#   Average ≥ $10 and < $20: "Good Savings Habits!"
+#   Average < $10: "Save More Consistently!"
+# 11 -add a data into set
+# 12 - Remove  a data from set
+# 13 - Join 2 set using union function
+# 14 - Print the common data in two sets
+# 15 - Access an item in dictionary
+# 16 - Add an item into dictionary
+# 17 - Delete an item from dictionary
+# 18 - Change an item from dictionary
+# 19 - Looping through dictionary
+# 20 - Copy the dictionary
+# 21 - Write a program to add a data into the tuple
+# 22 - Write a program to remove the mango from tuple
+# 23 - Write a program to remove the 3rd index data from tuple
+# 24 - Write a program to find the sum of even numbers between a range
+# 25 - Write a program to create a oops for the following details :
+#       -> Classes - 6
+#       ->  3 attributes for each class
+#        ->  4 object for each class
+# 26 - Write a program to show an example for single, multiple and multi - level inheritance
+# 27 - Write a program to show an example for polymorphism and encapsulation
