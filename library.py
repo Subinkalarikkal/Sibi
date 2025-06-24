36805e108432d3c3f7031dd659b78d465bacd43e
sibis <sinisenthilk@gmail.com>
2025-06-24 17:49:48 -0400

    update

---
+++ library.py
@@ -0,0 +1,18 @@
+#creation
+# import numpy as np
+# a=np.array([1,2,3,4,5])
+# print(a)
+
+#indexing
+# import numpy as np
+#
+# arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+#
+# print('2nd element on 1st row: ', arr[0, 1])
+
+#slicing
+# import numpy as np
+#
+# arr = np.array([1, 2, 3, 4, 5, 6, 7])
+#
+# print(arr[1:5])
